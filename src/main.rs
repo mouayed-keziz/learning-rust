343a612f7859feeb7e5a9ae3ff33561dea92218a
mouayed-keziz <m_keziz@estin.dz>
2024-02-25 19:16:38 +0100

    add multiplication table project

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
-    println!("Hello, world!");
+    let n = 12;
+    println!("-----------------");
+    for i in 1..n + 1 {
+        print_multiplication_table(i, n);
+        println!("-----------------");
+    }
+}
+
+fn print_multiplication_table(i: i32, n: i32) {
+    for j in 1..n + 1 {
+        println!("{} x {} = {}", i, j, i * j);
+    }
 }
